9cfcc7fde2fb1ca01ecddff7f6d8317e03676c47
Hans Naert <hans.naert@vives.be>
2023-11-25 15:22:44 +0100

    Inject the store in app.component and test with console.logs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookCollectionComponent } from './book-collection/book-collection.component';
+import { Store } from '@ngrx/store';
+import { BooksActions, BooksApiActions } from './State/books.actions';
+import { Observable } from 'rxjs';
+import { Book } from './Models/book.model';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +17,18 @@ import { BookCollectionComponent } from './book-collection/book-collection.compo
 })
 export class AppComponent {
   title = 'ngrx-walkthrough-standalone';
+
+  book$:Observable<ReadonlyArray<Book>>=this.store.select('books');
+  collection$:Observable<ReadonlyArray<String>>=this.store.select('collection');
+
+  constructor(private store: Store<{books: ReadonlyArray<Book>, collection: ReadonlyArray<String>}>) {     
+  }
+
+  ngOnInit() {
+    this.book$.subscribe(books => console.log(books));    
+    this.collection$.subscribe(collection => console.log(collection));
+    this.store.dispatch(BooksApiActions.retrievedBookList({ books: [{ id: '1', volumeInfo: { title: 'Book 1', authors: ['Piet', 'Luc'] } }, { id: '2', volumeInfo: { title: 'Book 2', authors:['Marc'] } }] }));
+    this.store.dispatch(BooksActions.addBook({ bookId: '1' }));
+  }
+
 }
