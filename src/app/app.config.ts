f8012e41d63086ed5000d1c006af61d1c083df23
Hans Naert <hans.naert@vives.be>
2023-11-25 15:21:43 +0100

    Add books.reducers and collection.reducers, provide them in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,11 @@ import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideStore } from '@ngrx/store';
 
+import { provideState } from '@ngrx/store';
+import { booksReducer } from './State/books.reducers';
+import { collectionReducer } from './State/collection.reducers';
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideStore()]
+  providers: [provideRouter(routes), provideAnimations(), provideStore(), provideState({name: 'books', reducer: booksReducer}), provideState({name: 'collection', reducer: collectionReducer})]
 };
