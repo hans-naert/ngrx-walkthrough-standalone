de0d9cf93767013ef76b1f30ad1e31ec9e6bf37c
Hans Naert <hans.naert@vives.be>
2023-11-27 09:40:24 +0100

    Add actions,reducers and do small test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Book } from '../models/books.model';
+import { BooksApiActions } from '../store/books.actions';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +14,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ngrx-walkthrough-standalone';
+
+  books$ = this.store.select('books');
+  collection$ = this.store.select('collection');
+
+  constructor(private store: Store<{books : ReadonlyArray<Book>, collection:ReadonlyArray<string>}>) {
+    this.store.dispatch(BooksApiActions.retrievedBookList({books: [{id: '1', volumeInfo: {title: 'Book 1', authors: ['Author 1']}},{id: '2', volumeInfo: {title: 'Book 2', authors: ['Author 1']}}]}));
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideStore } from '@ngrx/store';
+import { provideState } from '@ngrx/store';
+import { booksReducer } from '../store/books.reducer';
+import { collectionReducer } from '../store/collection.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideStore()]
+  providers: [provideRouter(routes), provideAnimations(), provideStore(), provideState({name:'books', reducer: booksReducer}), provideState({name:'collection', reducer: collectionReducer})]
 };
